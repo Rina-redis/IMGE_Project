e51b61f4bdfb33d85759e1706daadd28d423e212
rina redis <marinaredka1@gmail.com>
2024-11-08 14:23:52 +0100

    Third View Camera ver 2.0

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,10 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-
-
     [SerializeField]
     private InputActionReference movementControlls;
     [SerializeField]
@@ -13,16 +12,33 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-
+    [SerializeField]
+    private Transform cameraMainTransform;
 
     [SerializeField]
     private float playerSpeed = 2.0f;
     [SerializeField]
+    private float rotationSpeed = 2.0f;
 
+
+    [SerializeField]
     private float jumpHeight = 1.0f;
     [SerializeField]
 
     private float gravityValue = -9.81f;
+
+    public void OnEnable()
+    {
+        movementControlls.action.Enable();
+        jumpControlls.action.Enable();
+    }
+
+    public void OnDisable()
+    {
+        movementControlls.action.Disable();
+        jumpControlls.action.Disable();
+    }
+
 
     private void Start()
     {
@@ -39,15 +55,24 @@ public class PlayerController : MonoBehaviour
 
         Vector2 movement = movementControlls.action.ReadValue<Vector2>();
         Vector3 move = new Vector3(movement.x, 0, movement.y);
+        move = cameraMainTransform.forward * move.z + cameraMainTransform.right * move.x;
+        move.y = 0;
         controller.Move(move * Time.deltaTime * playerSpeed);
 
         // Makes the player jump
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        if (jumpControlls.action.triggered && groundedPlayer)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
         }
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+
+        if (movement != Vector2.zero)
+        {
+            float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg + cameraMainTransform.eulerAngles.y;
+            Quaternion targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+        }
     }
 }
